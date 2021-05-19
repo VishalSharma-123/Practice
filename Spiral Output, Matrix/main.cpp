23f397d93845a2744d8109f6541c66d0888756cd
VishalSharma-123 <79043294+VishalSharma-123@users.noreply.github.com>
2021-05-19 09:29:11 +0530

    Add files via upload

--- Spiral Output, Matrix/main.cpp
+++ Spiral Output, Matrix/main.cpp
@@ -2,42 +2,19 @@
 using namespace std;
 
 int main(){
-    int N;
+    int N,count=0;
     cin >> N;
-    int arr[N][N];
-    int i=0,j;
-    while (i<N){
-        j=0;
-        while (j<N){
-            cin >> arr[i][j];
-            j+=1;
+    while (N--){
+        string s;
+        cin >> s;
+        for (int i=0; i<s.length()-1; i++){
+            string str=s.substr(i,2);
+            if (str == "ch" || str == "he" || str == "ef"){
+                count ++;
+                break;
+            }
         }
-        i+=1;
     }
-
-    int m = N;
-    int r=0, c=0;
-    
-    while (r<=(N/2)){
-        for (int a = r; a<m; a++){
-            printf("%d",arr[r][a]);
-        }
-
-        for (int a=c+1; a<m; a++){
-            printf("%d",arr[a][m-1]);
-        }
-
-        for (int a = m-1; a>=r; a--){
-            printf("%d",arr[m-1][a]);
-        }
-
-        for (int a = m-2; a>r; a--){
-            printf("%d",arr[a][m-2]);
-        }
-
-        r+=1;
-        c+=1;
-        m-=1;
-    }
+    cout << count << endl;    
     return 0;
 }
